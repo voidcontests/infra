b988d6ad184a9e6ce951253c8996663f0c167102
jus1d <artfa63@gmail.com>
2025-10-30 16:21:24 +0400

    ci: add script for pushing docker images

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME=$1
+if [ -z "$IMAGE_NAME" ]; then
+  echo "Usage: $0 <image_name>"
+  exit 1
+fi
+
+SHORT_SHA=${GITHUB_SHA::7}
+BRANCH_NAME=${GITHUB_REF#refs/heads/}
+TAG_NAME=${GITHUB_REF#refs/tags/}
+
+if [[ -n "$TAG_NAME" ]]; then
+  echo "Building and pushing image with tag: $TAG_NAME"
+  docker build -t $IMAGE_NAME:$TAG_NAME .
+  docker push $IMAGE_NAME:$TAG_NAME
+else
+  echo "Building image with short SHA and branch name: $SHORT_SHA, $BRANCH_NAME"
+  docker build -t $IMAGE_NAME:$SHORT_SHA -t $IMAGE_NAME:$BRANCH_NAME .
+  if [[ "$BRANCH_NAME" == "master" || "$BRANCH_NAME" == "dev" ]]; then
+    docker push $IMAGE_NAME:$SHORT_SHA
+  fi
+  docker push $IMAGE_NAME:$BRANCH_NAME
+
+  if [[ "$BRANCH_NAME" == "master" ]]; then
+    docker tag $IMAGE_NAME:$SHORT_SHA $IMAGE_NAME:latest
+    docker push $IMAGE_NAME:latest
+  fi
+fi
